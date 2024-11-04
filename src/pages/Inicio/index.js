292efa92fb302c5c9d8198f44bfd16b290a12fa5
joaolucas14 <jlparceiro10@gmail.com>
2024-11-04 16:58:46 -0300

    adicionando o react router dom e criando alguns componentes

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,12 @@
+import Cabecalho from "componentes/Cabecalho";
+import Rodape from "componentes/Rodape";
+function Inicio() {
+  return (
+    <>
+      <Cabecalho></Cabecalho>
+      <Rodape />
+    </>
+  );
+}
+
+export default Inicio;
