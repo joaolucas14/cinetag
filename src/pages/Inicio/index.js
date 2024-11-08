f92516294ef523445d4ed658fe1becdb841e79ed
joaolucas14 <jlparceiro10@gmail.com>
2024-11-08 17:18:37 -0300

    criando os cards de maneira dinamica

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,11 @@
 import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
+import Card from "componentes/Card";
 import Rodape from "componentes/Rodape";
 import Titulo from "componentes/Titulo";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
+
 function Inicio() {
   return (
     <>
@@ -10,6 +14,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus videos e filmes!</h1>
       </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.video}></Card>;
+        })}
+      </section>
       <Rodape />
     </>
   );
